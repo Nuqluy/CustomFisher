cfe70bc75f8f2daff082e0f627d5a0e855ce7aac
Nuqluy <berrycharlie055@gmail.com>
2023-11-06 15:33:04 +0000

    Initialisation

---
+++ src/main/java/nuqluy/customfisher/CustomFisher.java
@@ -0,0 +1,17 @@
+package nuqluy.customfisher;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomFisher extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
